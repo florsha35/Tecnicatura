b672a3ce1413afffcb23e8989817df9d170a22f1
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-04-25 19:23:03 -0300

    mi p

---
+++ Phyton/Leccion1/main.py
@@ -0,0 +1,10 @@
+miVariable: int = 3
+print(miVariable)
+miVariable = "Hello all student of this class"
+print(miVariable)
+miVariable = 3.
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
