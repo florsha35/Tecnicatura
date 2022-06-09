c84c3c4e283b875694649eb3a2807c987a2bed84
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-06-08 21:23:48 -0300

    ingresamos otra actualizacion de los archivos de Phyton

--- Phyton/Leccion2/Ejercicio1.py
+++ Phyton/Leccion2/Ejercicio1.py
@@ -40,8 +40,53 @@
 # print(f'El area : {area}')
 # longitud = 2 * import math * radio
 # print(f'La longitud es: {longitud}')
-#
 
+
+
+# Ejericio Clase 7
+# mes = int(input('Digite un mes del año entre 1-12:  '))
+# verano = mes <=3
+# otoño = mes >3 and mes <=6
+# invierno = mes >6 and mes <=9
+# primavera = mes >9
+# if verano:
+#    print('La estacion del año es VERANO')
+# elif otoño:
+#    print('La estacion del año es OTOÑO')
+# elif invierno:
+#    print('La estacion del año es invierno')
+# else:
+#    print('La estación es PRIMAVERA')
+
+#edad = int(input('Digite su edad (0/29): '))
+#infancia = edad <= 10
+#cambios = edad > 10 and edad <= 19
+#adulto = edad = 20 or edad >= 29
+#if infancia:
+#    print('La infancia es increible y bella')
+#elif cambios:
+#    print('Tienes muchos cambios, mucho que aprender')
+#elif adulto:
+#    print('Amor y comienza el trabajo')
+#else:
+#    print('Numero fuera de rango')
+
+#Sistema de calificaciones
+
+calif = float(input('digite un valor de calificacion (0-10): '))
+
+if   9 <= calif <= 10:
+    print('A')
+elif 8 <= calif <9:
+    print('B')
+elif 7 <= calif <8:
+    print('C')
+elif 6<= calif <7:
+    print('D')
+elif 0 <= calif <6:
+    print('F')
+else:
+     print('El valor ingresado es incorrecto')
 
 
 
