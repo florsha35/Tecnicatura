5327aad0ee033603671c654df95100592096f739
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-06-21 15:29:37 -0300

    nuevo proyecto ejercicio7, calculo de salario vendedor de auto

--- Java/Ejercicio_7/src/Ejercicio_7/Ejercicio_7.java
+++ Java/Ejercicio_7/src/Ejercicio_7/Ejercicio_7.java
@@ -6,23 +6,23 @@ public class Ejercicio_7 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        final int salario = 1000;
-        int comision = 150, venta;
-        float salarioMensual, ventaCarro, comVenta, totalPrecio;
-       
-        
-        System.out.print("Digite la cantidad de autos vendidos: ");
-        venta = Integer.parseInt(entrada.nextLine());
-        System.out.println("Digite el valor del carro: ");
-        ventaCarro = Integer.parseInt(entrada.nextLine());
-        
-        comision *= venta;
-        totalPrecio = ventaCarro * venta;
-        comVenta = totalPrecio * 0.05f;
-        salarioMensual = salario + comision + comVenta;
-        
-        
-        System.out.println("El salario mensual es = " + salarioMensual);
+//        final int salario = 1000;
+//        int comision = 150, venta;
+//        float salarioMensual, ventaCarro, comVenta, totalPrecio;
+//       
+//        
+//        System.out.print("Digite la cantidad de autos vendidos: ");
+//        venta = Integer.parseInt(entrada.nextLine());
+//        System.out.println("Digite el valor del carro: ");
+//        ventaCarro = Integer.parseInt(entrada.nextLine());
+//        
+//        comision *= venta;
+//        totalPrecio = ventaCarro * venta;
+//        comVenta = totalPrecio * 0.05f;
+//        salarioMensual = salario + comision + comVenta;
+//        
+//        
+//        System.out.println("El salario mensual es = " + salarioMensual);
         
     }
     
