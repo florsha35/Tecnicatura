b6f4cf188a44c6ab3b013ebf64edd0e56b7aa9f8
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-06-21 14:52:08 -0300

    agregamos aparte ejercicio cuatro calculo de numero mayor

--- Java/Ejercicio_4/src/Ejercicio4/Ejercicio4.java
+++ Java/Ejercicio_4/src/Ejercicio4/Ejercicio4.java
@@ -7,12 +7,12 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.print("Digite un numero   ");
-        int num1 = Integer.parseInt(entrada.nextLine());
-        System.out.print("Digite otro numero   ");
-        int num2 = Integer.parseInt(entrada.nextLine());
-        System.out.print("El numero mayor es:  ");
-        System.out.print(num1 > num2 ?  num1 : num2);
+//        System.out.print("Digite un numero   ");
+//        int num1 = Integer.parseInt(entrada.nextLine());
+//        System.out.print("Digite otro numero   ");
+//        int num2 = Integer.parseInt(entrada.nextLine());
+//        System.out.print("El numero mayor es:  ");
+//        System.out.print(num1 > num2 ?  num1 : num2);
         
                 
      }
