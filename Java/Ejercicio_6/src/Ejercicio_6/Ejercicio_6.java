b77d200867a232ee40a8250bb096bad6adf53357
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-06-21 15:28:01 -0300

    agregamos en nuevo proyecto con ejercicio6, calculo de dolares

--- Java/Ejercicio_6/src/Ejercicio_6/Ejercicio_6.java
+++ Java/Ejercicio_6/src/Ejercicio_6/Ejercicio_6.java
@@ -7,19 +7,19 @@ public class Ejercicio_6 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
                 
-        float Guillermo, Juan, Luis, suma ;
-        
-        System.out.println("Digite el monto de dolares que tiene Guillermo");
-        Guillermo = Float.parseFloat(entrada.nextLine());
-        
-        Luis = (Guillermo / 2);
-        System.out.println("Luis tiene dolares = " + Luis );
-                 
-        Juan = (Guillermo + Luis) / 2;
-        System.out.println("Juan tiene  dolares = " + Juan );
-        
-        suma = Guillermo + Luis + Juan;
-        System.out.println("Entre Guillermo, Luis y Juan, tienen dolares = " + suma);
+//        float Guillermo, Juan, Luis, suma ;
+//        
+//        System.out.println("Digite el monto de dolares que tiene Guillermo");
+//        Guillermo = Float.parseFloat(entrada.nextLine());
+//        
+//        Luis = (Guillermo / 2);
+//        System.out.println("Luis tiene dolares = " + Luis );
+//                 
+//        Juan = (Guillermo + Luis) / 2;
+//        System.out.println("Juan tiene  dolares = " + Juan );
+//        
+//        suma = Guillermo + Luis + Juan;
+//        System.out.println("Entre Guillermo, Luis y Juan, tienen dolares = " + suma);
         
         
         
