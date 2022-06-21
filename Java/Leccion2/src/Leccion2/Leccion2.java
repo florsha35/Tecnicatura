d08f60880dca4803b5840d1c0deac81c20cd9832
Florencia <florshapasnikoff@gmail.comgit config --lcoal user.name florencia>
2022-06-21 15:30:57 -0300

    creamos la Leccion 2 y hacemos dos ejercicios condicionales

--- Java/Leccion2/src/Leccion2/Leccion2.java
+++ Java/Leccion2/src/Leccion2/Leccion2.java
@@ -11,24 +11,24 @@ public class Leccion2 {
 //            System.out.println("Condicion Falsa"); // Condicion Doble
 //        }
         
-//        var numero = 4;
-//        var numeroTexto = "numero desconocido";
-//        if (numero == 1) {
-//            numeroTexto = "numero uno";
-//        }
-//        else if (numero == 2){
-//            numeroTexto = "numero dos";
-//        }
-//        else if (numero ==3){
-//            numeroTexto = "numero tres";
-//        }
-//        else if (numero == 4){
-//            numeroTexto = "numero cuatro";
-//        }
-//        else{
-//            numeroTexto = "numero no encontrado";
-//        }
-//        System.out.println("numeroTexto = " + numeroTexto);
+        var numero = 4;
+        var numeroTexto = "numero desconocido";
+        if (numero == 1) {
+            numeroTexto = "numero uno";
+        }
+        else if (numero == 2){
+            numeroTexto = "numero dos";
+        }
+        else if (numero ==3){
+            numeroTexto = "numero tres";
+        }
+        else if (numero == 4){
+            numeroTexto = "numero cuatro";
+        }
+        else{
+            numeroTexto = "numero no encontrado";
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
 
 
 
