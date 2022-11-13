967bf79185b2da08ef5c242fea2fd26dadeb4da5
Florencia Shapasnikoff <florshapasnikoff@gmail.com>
2022-11-13 12:10:48 -0300

    importamos la clase Persona2 creada previamente y elimibamos objetos

---
+++ Phyton/Leccion6/PruebaPersona2.py
@@ -0,0 +1,11 @@
+from Persona2 import Persona2
+print('Creacion de objetos'.center(50, '-'))
+
+if __name__== '__main__':
+    persona4 = Persona2('Lionel', 'Messi', 35)
+    persona4.mostrar_detalles()
+
+    print(__name__)
+
+print('Eliminacion de objetos'.center(50, '*'))
+del persona4 
